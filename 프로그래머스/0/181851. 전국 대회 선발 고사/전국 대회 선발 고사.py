9c22e88ab7a1e09c68788fa587c5658621901b14
Eui Joong Kim <158041455+kimdevspace@users.noreply.github.com>
2024-02-03 16:47:31 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.py
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.py
@@ -1,11 +1,3 @@
 def solution(rank, attendance):
-    a=[]
-    aa=[]
-    for i,b in zip(rank,attendance):
-        if b==True:
-            a.append(i)
-    a=sorted(a)
-    for i in range(3):
-        aa.append(a[i])
-    answer=rank.index(aa[0])*10000+rank.index(aa[1])*100+rank.index(aa[2])
-    return answer
+    a=sorted([i for i,b in zip(rank,attendance) if b==True])
+    return rank.index(a[0])*10000+rank.index(a[1])*100+rank.index(a[2])
