760c61776bfa15889f8bbef74a99366a09651cb4
Eui Joong Kim <158041455+kimdevspace@users.noreply.github.com>
2024-02-09 19:54:48 +0900

    [level 1] Title: 소수 찾기, Time: 2947.76 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -0,0 +1,11 @@
+def isPrime(a):
+    for i in range(2,int(a**0.5)+1):
+        if a%i==0:
+            return False
+    return True
+def solution(n):
+    answer = 0
+    for i in range(2,n+1):
+        if isPrime(i)==True:
+            answer+=1
+    return answer
