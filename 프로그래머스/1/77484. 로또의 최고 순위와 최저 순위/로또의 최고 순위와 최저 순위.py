c4a9a7c9f52a6dda57e43da19e65be2923dbf9eb
Eui Joong Kim <158041455+kimdevspace@users.noreply.github.com>
2024-02-12 14:29:01 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
@@ -1,10 +1,10 @@
 def solution(lottos, win_nums):
-    d={6:1,5:2,4:3,3:4,2:5,1:6,0:6}
-    same=[]
+    rank_dic={6:1,5:2,4:3,3:4,2:5,1:6,0:6}
+    same=0
     for i in lottos:
         for j in win_nums:
             if i==j:
-                same.append(i)
-    max_rank=d[len(same)+lottos.count(0)]
-    min_rank=d[len(same)]
+                same+=1
+    max_rank=rank_dic[same+lottos.count(0)]
+    min_rank=rank_dic[same]
     return [max_rank,min_rank]
