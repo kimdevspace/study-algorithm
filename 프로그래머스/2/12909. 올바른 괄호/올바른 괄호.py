e6c38e2bdf9cdff8d24fab5bf205fa1286af514f
Eui Joong Kim <158041455+kimdevspace@users.noreply.github.com>
2024-02-13 21:31:30 +0900

    [level 2] Title: 올바른 괄호, Time: 7.67 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -3,8 +3,9 @@ def solution(s):
     for i in s:
         if i=='(':
             stack.append(i)
-        elif i==')' and len(stack)==0:
-            stack.append(i)
-        elif i==')' and len(stack)>0:
-            stack.pop()
-    return True if len(stack)==0 else False
+        elif i==')':
+            try:
+                stack.pop()
+            except IndexError:
+                return False
+    return len(stack)==0
