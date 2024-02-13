902768353cb97572d0a77a9a89976f7d109718c3
Eui Joong Kim <158041455+kimdevspace@users.noreply.github.com>
2024-02-13 16:54:33 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.54 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
@@ -2,10 +2,10 @@ def solution(board, moves):
     answer = 0
     basket=[]
     for m in moves:
-        for boa in board:
-            if boa[m-1]>0:
-                basket.append(boa[m-1])
-                boa[m-1]=0
+        for b in board:
+            if b[m-1]>0:
+                basket.append(b[m-1])
+                b[m-1]=0
                 break
         if len(basket)>=2 and basket[-1]==basket[-2]:
             answer+=2
