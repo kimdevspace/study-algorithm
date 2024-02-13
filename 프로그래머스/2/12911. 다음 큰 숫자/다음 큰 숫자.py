b8b276d4a574e03daaa4cd864785ab9a51f3bafd
Eui Joong Kim <158041455+kimdevspace@users.noreply.github.com>
2024-02-13 22:27:46 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -1,7 +1,7 @@
 def solution(n):
-    count=str(bin(n)[2:]).count('1')
+    count=bin(n).count('1')
     while n:
         n+=1
-        if str(bin(n)[2:]).count('1')==count:
+        if bin(n).count('1')==count:
             break
     return n
