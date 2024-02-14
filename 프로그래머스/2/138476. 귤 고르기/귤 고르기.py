c22920d76f6401db881ba81ec800df557ed4c67f
Eui Joong Kim <158041455+kimdevspace@users.noreply.github.com>
2024-02-14 23:12:34 +0900

    [level 2] Title: 귤 고르기, Time: 20.18 ms, Memory: 17.9 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -1,10 +1,10 @@
 from collections import Counter
 def solution(k, tangerine):
     answer = 0
-    cnt = Counter(tangerine)
+    cnt=Counter(tangerine)
     for i in sorted(cnt.values(),reverse=True):
-        k-=i
         answer+=1
+        k-=i
         if k<=0:
             break
     return answer
