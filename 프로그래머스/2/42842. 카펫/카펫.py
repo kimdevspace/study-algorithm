58c7369cafe42e3832ff281e7ee3547151036b52
Eui Joong Kim <158041455+kimdevspace@users.noreply.github.com>
2024-02-14 17:21:44 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,8 +1,7 @@
 def solution(brown, yellow):
     ab=brown+yellow
     for b in range(1,ab+1):
-        if (ab/b)%1==0:
-            a=ab/b
-            if a>=b:
-                if (a*2+b*2)-4==brown:
-                    return [a,b]
+        a=ab/b
+        if a>=b:
+            if (a*2+b*2)-4==brown:
+                return [a,b]
