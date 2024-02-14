5e52fdddef6a8254301c91fa2c052d7145644106
Eui Joong Kim <158041455+kimdevspace@users.noreply.github.com>
2024-02-15 00:29:15 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 320.70 ms, Memory: 43.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -0,0 +1,11 @@
+def solution(elements):
+    n=len(elements)
+    dp=elements
+    elements=2*elements
+    answer=set(dp)
+    for i in range(2,n):
+        for j in range(n):
+            dp[j]=dp[j]+elements[i+j-1]
+            answer.add(dp[j])
+    return len(answer)+1
+    
