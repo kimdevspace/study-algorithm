dc7da85fc193bbfbb30272a83efba45625214ea8
Eui Joong Kim <158041455+kimdevspace@users.noreply.github.com>
2024-02-15 12:42:57 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -1,6 +1,6 @@
 from math import gcd
 def solution(arr):
     answer = arr[0]
-    for num in arr:
-        answer=answer*num//gcd(answer,num) # 최대공약수 = a x b // gcd(a,b)
+    for i in arr:
+        answer=answer*i//gcd(answer,i)
     return answer
