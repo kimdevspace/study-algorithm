7845820a04d43fbda55996399906710144b6e729
Eui Joong Kim <158041455+kimdevspace@users.noreply.github.com>
2024-02-15 14:43:16 +0900

    [level 2] Title: 할인 행사, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -0,0 +1,11 @@
+def solution(want, number, discount):
+    n=len(discount)
+    answer = 0
+    for i in range(n-9):
+        dic={w:0 for w in want}
+        for j in range(i,i+10):
+            if discount[j] in dic:
+                dic[discount[j]]+=1
+        if list(dic.values())==number:
+            answer+=1
+    return answer
