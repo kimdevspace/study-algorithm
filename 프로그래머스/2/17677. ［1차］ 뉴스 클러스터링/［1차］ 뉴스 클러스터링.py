a6d7ce932392d1fea3009ac2886014a2d9320c54
Eui Joong Kim <158041455+kimdevspace@users.noreply.github.com>
2024-02-18 17:38:45 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.08 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
@@ -2,16 +2,8 @@ from collections import Counter
 def solution(str1, str2):
     str1=str1.lower()
     str2=str2.lower()
-    str1_lst=[]
-    str2_lst=[]
-    for i in range(len(str1)-1):
-        if str1[i].isalpha() and str1[i+1].isalpha():
-            str1_lst.append(str1[i]+str1[i+1])
-    for i in range(len(str2)-1):
-        if str2[i].isalpha() and str2[i+1].isalpha():
-            str2_lst.append(str2[i]+str2[i+1])
-    c1=Counter(str1_lst)
-    c2=Counter(str2_lst)
+    c1=Counter([str1[i]+str1[i+1] for i in range(len(str1)-1) if str1[i].isalpha() and str1[i+1].isalpha()])
+    c2=Counter([str2[i]+str2[i+1] for i in range(len(str2)-1) if str2[i].isalpha() and str2[i+1].isalpha()])
     inter=list((c1&c2).elements())
     union=list((c1|c2).elements())
     if len(inter)==0 and len(union)==0:
