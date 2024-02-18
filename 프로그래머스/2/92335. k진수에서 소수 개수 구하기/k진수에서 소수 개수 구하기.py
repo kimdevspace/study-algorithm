5fced8a4320fe1d506bafa132bb470c107da7dad
Eui Joong Kim <158041455+kimdevspace@users.noreply.github.com>
2024-02-18 20:09:33 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
@@ -0,0 +1,18 @@
+def solution(n, k):
+    word=''
+    while n:
+        word=str(n%k)+word
+        n//=k
+    word=word.split('0')
+    cnt=0
+    for w in word:
+        if len(w)==0 or int(w)<2:
+            continue
+        sosu=True
+        for i in range(2,int(int(w)**0.5)+1):
+            if int(w)%i==0:
+                sosu=False
+                break
+        if sosu:
+            cnt+=1
+    return cnt
