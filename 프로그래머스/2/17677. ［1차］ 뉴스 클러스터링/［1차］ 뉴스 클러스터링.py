2d741b6c6abfc6b81d14a941b401477b0254a61b
Eui Joong Kim <158041455+kimdevspace@users.noreply.github.com>
2024-02-18 17:39:58 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.11 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
@@ -1,9 +1,7 @@
 from collections import Counter
 def solution(str1, str2):
-    str1=str1.lower()
-    str2=str2.lower()
-    c1=Counter([str1[i]+str1[i+1] for i in range(len(str1)-1) if str1[i].isalpha() and str1[i+1].isalpha()])
-    c2=Counter([str2[i]+str2[i+1] for i in range(len(str2)-1) if str2[i].isalpha() and str2[i+1].isalpha()])
+    c1=Counter([(str1[i]+str1[i+1]).lower() for i in range(len(str1)-1) if str1[i].isalpha() and str1[i+1].isalpha()])
+    c2=Counter([(str2[i]+str2[i+1]).lower() for i in range(len(str2)-1) if str2[i].isalpha() and str2[i+1].isalpha()])
     inter=list((c1&c2).elements())
     union=list((c1|c2).elements())
     if len(inter)==0 and len(union)==0:
