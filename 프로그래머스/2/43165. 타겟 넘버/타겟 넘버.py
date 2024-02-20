efd54c5fae0b411e767b161771f0598a66233a44
Eui Joong Kim <158041455+kimdevspace@users.noreply.github.com>
2024-02-20 12:56:39 +0900

    [level 2] Title: 타겟 넘버, Time: 4.60 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,14 +1,15 @@
+###DFS
 answer=0
-def dfs(idx,value,numbers,target):
+def dfs(idx,val,numbers,target):
     global answer
     #탈출조건
     if idx==len(numbers):
-        if value==target:
+        if val==target:
             answer+=1
         return
     #수행동작
-    dfs(idx+1,value+numbers[idx],numbers,target)
-    dfs(idx+1,value-numbers[idx],numbers,target)
+    dfs(idx+1,val+numbers[idx],numbers,target)
+    dfs(idx+1,val-numbers[idx],numbers,target)
 def solution(numbers, target):
     dfs(0,0,numbers,target)
     return answer
