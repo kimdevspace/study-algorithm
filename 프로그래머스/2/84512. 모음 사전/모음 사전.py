290b786a1a508a2bda76a174be48c3109623e829
Eui Joong Kim <158041455+kimdevspace@users.noreply.github.com>
2024-02-22 11:32:33 +0900

    [level 2] Title: 모음 사전, Time: 1.37 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,8 +1,8 @@
 from itertools import product
 def solution(word):
-    words=[]
+    answer = []
     for i in range(1,6):
-        for pro in product(['A','E','I','O','U'],repeat=i):
-            words.append(''.join(list(pro)))
-    words.sort()
-    return words.index(word)+1
+        for p in product(['A','E','I','O','U'],repeat=i):
+            answer.append(''.join(list(p)))
+    answer.sort()
+    return answer.index(word)+1
