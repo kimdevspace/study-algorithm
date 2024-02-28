17e0d376bd2fb54a6234ba5fc22a327bca771058
Eui Joong Kim <158041455+kimdevspace@users.noreply.github.com>
2024-02-28 15:09:39 +0900

    [level 2] Title: 롤케이크 자르기, Time: 581.47 ms, Memory: 51.4 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -1,13 +1,13 @@
 from collections import Counter
 def solution(topping):
-    answer = 0
+    answer=0
     dic=Counter(topping)
-    set_dic=set()
+    div=set()
     for t in topping:
         dic[t]-=1
-        set_dic.add(t)
+        div.add(t)
         if dic[t]==0:
             dic.pop(t)
-        if len(dic)==len(set_dic):
+        if len(dic)==len(div):
             answer+=1
     return answer
