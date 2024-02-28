2ded728dfcd98ded1fb41a6046e5aecb975423c0
Eui Joong Kim <158041455+kimdevspace@users.noreply.github.com>
2024-02-28 13:44:55 +0900

    [level 2] Title: 방문 길이, Time: 0.25 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.py
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.py
@@ -1,12 +1,12 @@
 def solution(dirs):
     answer = 0
-    dic={'U':(0,1),'D':(0,-1),'L':(-1,0),'R':(1,0)}
+    distance={'U':(0,1),'D':(0,-1),'L':(-1,0),'R':(1,0)}
     visited=set()
-    x,y=0,0
+    x=y=0
     for d in dirs:
-        nx,ny=x+dic[d][0],y+dic[d][1]
+        nx,ny=x+distance[d][0],y+distance[d][1]
         if -5<=nx<=5 and -5<=ny<=5:
-            visited.add((x,y,nx,ny))
             visited.add((nx,ny,x,y))
+            visited.add((x,y,nx,ny))
             x,y=nx,ny
     return len(visited)//2
