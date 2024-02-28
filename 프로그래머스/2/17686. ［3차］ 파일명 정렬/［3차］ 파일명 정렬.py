904bfee1cabdb86de74a6ca48c73276578ae126c
Eui Joong Kim <158041455+kimdevspace@users.noreply.github.com>
2024-02-28 16:55:23 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 2.72 ms, Memory: 10.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -0,0 +1,17 @@
+def solution(files):
+    answer = []
+    for file in files:
+        head,number,tail='','',''
+        for i in range(len(file)):
+            if file[i].isdigit():
+                head=file[:i]
+                number=file[i:]
+                for j in range(len(number)):
+                    if not number[j].isdigit():
+                        tail=number[j:]
+                        number=number[:j]
+                        break
+                answer.append([head,number,tail])
+                break
+    answer=sorted(answer,key=lambda x:(x[0].upper(),int(x[1])))
+    return [''.join(i) for i in answer]
