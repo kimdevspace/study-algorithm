64e2cdd8bc13aaa83c3e4180abec98344e566bc8
Eui Joong Kim <158041455+kimdevspace@users.noreply.github.com>
2024-02-28 13:40:02 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.py
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.py
@@ -1,21 +1,22 @@
+
 from math import ceil
 def htom(time):
     h,m=map(int,time.split(':'))
     return h*60+m
 def solution(fees, records):
     answer = []
-    par={}
+    park={}
     res={}
     for r in records:
         time,num,io=r.split()
         if io=='IN':
-            par[num]=htom(time)
+            park[num]=htom(time)
             if num not in res:
                 res[num]=0
         else:
-            res[num]+=htom(time)-par[num]
-            del par[num]
-    for k,v in par.items():
+            res[num]+=htom(time)-park[num]
+            del park[num]
+    for k,v in park.items():
         res[k]+=23*60+59-v
     for k,v in sorted(res.items()):
         if v<=fees[0]:
