d6790f7572eaeb3f33d9ee506f35cbc9931b1a80
Eui Joong Kim <158041455+kimdevspace@users.noreply.github.com>
2024-02-29 22:16:25 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 58.68 ms, Memory: 12.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.py
@@ -0,0 +1,17 @@
+def solution(arr):
+    answer = [0,0]
+    n=len(arr)
+    def comp(a,b,n):
+        start=arr[a][b]
+        for i in range(a,a+n):
+            for j in range(b,b+n):
+                if arr[i][j]!=start:
+                    n//=2
+                    comp(a,b,n)
+                    comp(a,b+n,n)
+                    comp(a+n,b,n)
+                    comp(a+n,b+n,n)
+                    return
+        answer[start]+=1
+    comp(0,0,n)
+    return answer
