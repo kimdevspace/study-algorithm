4a0ae2e367b7883838db13073a4baaec6eb939fb
Eui Joong Kim <158041455+kimdevspace@users.noreply.github.com>
2024-03-01 14:26:16 +0900

    [level 2] Title: 2 x n 타일링, Time: 11.82 ms, Memory: 11.8 MB -BaekjoonHub

--- 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.py
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.py
@@ -1,6 +1,6 @@
 def solution(n):
     dp=[0]*n
     dp[0],dp[1]=1,2
-    for i in range(2,n):
-        dp[i]=(dp[i-1]+dp[i-2])%1000000007
+    for i in range(2,len(dp)):
+        dp[i]=(dp[i-2]+dp[i-1])%1000000007
     return dp[i]
