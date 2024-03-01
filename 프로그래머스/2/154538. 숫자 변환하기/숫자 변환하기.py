03b0cef6ff3d38441d7b8304a9867e643aa85c41
Eui Joong Kim <158041455+kimdevspace@users.noreply.github.com>
2024-03-01 14:22:22 +0900

    [level 2] Title: 숫자 변환하기, Time: 6.98 ms, Memory: 17.1 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
@@ -1,13 +1,13 @@
 from collections import deque
 def solution(x, y, n):
     edge=[0]*(y+1)
-    q=deque([x])
-    while q:
-        tmp=q.popleft()
+    que=deque([x])
+    while que:
+        tmp=que.popleft()
         if tmp==y:
             return edge[tmp]
         for num in [tmp+n,tmp*2,tmp*3]:
             if num<=y and not edge[num]:
-                q.append(num)
+                que.append(num)
                 edge[num]=edge[tmp]+1
     return -1
