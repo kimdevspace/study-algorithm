2ca46d5ed086ba0de48fde5186a5d418991a2785
Eui Joong Kim <158041455+kimdevspace@users.noreply.github.com>
2024-03-02 19:32:42 +0900

    [Silver II] Title: 최소 힙, Time: 120 ms, Memory: 37044 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -0,0 +1,15 @@
+import sys
+import heapq as hq
+input=sys.stdin.readline
+num_list=[]
+hq.heapify(num_list)
+n=int(input())
+for _ in range(n):
+    x=int(input())
+    if x==0:
+        if num_list:
+            print(hq.heappop(num_list))
+        else:
+            print(0)
+    elif x!=0:
+        hq.heappush(num_list,x)
