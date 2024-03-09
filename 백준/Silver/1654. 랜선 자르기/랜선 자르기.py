127d87c7cee4609c1c1f50053d6aae6ee1f7f169
Eui Joong Kim <158041455+kimdevspace@users.noreply.github.com>
2024-03-09 14:14:03 +0900

    [Silver II] Title: 랜선 자르기, Time: 560 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -0,0 +1,15 @@
+k,n=map(int,input().split())
+lan=[]
+for _ in range(k):
+    lan.append(int(input()))
+start,end=1,max(lan)
+while start<=end:
+    mid=(start+end)//2
+    lines=0
+    for i in lan:
+        lines+=i//mid
+    if lines>=n:
+        start=mid+1
+    else:
+        end=mid-1
+print(end)
