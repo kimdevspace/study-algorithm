fffad8e2ac2541d98b47210b181103e2947fb226
Eui Joong Kim <158041455+kimdevspace@users.noreply.github.com>
2024-03-11 14:52:59 +0900

    [Silver V] Title: 집합, Time: 3512 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -25,6 +25,6 @@ for _ in range(M):
         else:
             S.add(com[1])
     elif com[0]=='all':
-        S=set(str(i) for i in range(1,21))
+        S=set(['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20'])
     elif com[0]=='empty':
         S=set()
