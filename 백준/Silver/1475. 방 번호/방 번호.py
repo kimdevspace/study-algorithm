4f1cd0fc58761dedf30cfc2022b53495086a5c4a
Eui Joong Kim <158041455+kimdevspace@users.noreply.github.com>
2024-03-11 15:35:42 +0900

    [Silver V] Title: 방 번호, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1475. 방 번호/방 번호.py
@@ -0,0 +1,12 @@
+n=input()
+lst=[0]*10
+for i in range(len(n)):
+    num=int(n[i])
+    if num==6 or num==9:
+        if lst[6]<=lst[9]:
+            lst[6]+=1
+        else:
+            lst[9]+=1
+    else:
+        lst[num]+=1
+print(max(lst))
