53be6f869d3d87817a57d370a9a6e53f54fa2d8a
Eui Joong Kim <158041455+kimdevspace@users.noreply.github.com>
2024-03-27 13:57:49 +0900

    [Silver II] Title: DFS와 BFS, Time: 528 ms, Memory: 39596 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,28 +1,34 @@
 from collections import deque
 n,m,v=map(int,input().split())
+#인접행렬생성
 graph=[[False]*(n+1) for _ in range(n+1)]
-visited1=[False]*(n+1)
-visited2=[False]*(n+1)
-for i in range(m):
+for _ in range(m):
     x,y=map(int,input().split())
     graph[x][y]=1
     graph[y][x]=1
+
+##dfs
+vis1=[False]*(n+1) #dfs 방문확인
 def dfs(v):
-    visited1[v]=True
-    print(v,end=' ')
+    vis1[v]=True #방문
+    print(v,end=' ') #방문한 곳 출력
     for i in range(1,n+1):
-        if not visited1[i] and graph[v][i]==1:
+        if not vis1[i] and graph[v][i]==1:
             dfs(i)
+
+##bfs
+vis2=[False]*(n+1)
 def bfs(v):
     q=deque([v])
-    visited2[v]=True #방문노드
+    vis2[v]=True
     while q:
         v=q.popleft()
         print(v,end=' ')
         for i in range(1,n+1):
-            if not visited2[i] and graph[v][i]==1:
+            if not vis2[i] and graph[v][i]==1:
                 q.append(i)
-                visited2[i]=True
+                vis2[i]=True
+
 dfs(v)
 print()
 bfs(v)
