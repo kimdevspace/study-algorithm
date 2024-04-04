0d0e26687f6b7857e3c5dd9640c75df784daf403
Eui Joong Kim <158041455+kimdevspace@users.noreply.github.com>
2024-04-04 11:55:44 +0900

    [level 3] Title: 네트워크, Time: 0.20 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -0,0 +1,14 @@
+def solution(n, computers):
+    def dfs(i):
+        visited[i]=1
+        for a in range(n):
+            if computers[i][a] and not visited[a]:
+                dfs(a)
+    
+    answer = 0
+    visited=[0 for _ in range(n)]
+    for i in range(n):
+        if not visited[i]:
+            dfs(i)
+            answer+=1
+    return answer
