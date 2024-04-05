2c15bedb9ab7d17dde957c6358be95a7392b8668
Eui Joong Kim <158041455+kimdevspace@users.noreply.github.com>
2024-04-05 16:34:39 +0900

    [Silver II] Title: 섬의 개수, Time: 80 ms, Memory: 31452 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.py
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.py
@@ -1,25 +1,24 @@
-from collections import deque
+import sys
+sys.setrecursionlimit(10000)
 dx=[-1,-1,-1,0,0,1,1,1]
 dy=[-1,0,1,-1,1,-1,0,1]
-def bfs(x,y):
-    q=deque([(x,y)])
-    arr[x][y]=0
-    while q:
-        x,y=q.popleft()
+def dfs(x,y):
+    visited[x][y]=1
+    if arr[x][y]==1:
         for i in range(8):
             nx,ny=x+dx[i],y+dy[i]
-            if 0<=nx<h and 0<=ny<w and arr[nx][ny]==1:
-                arr[nx][ny]=0
-                q.append((nx,ny))
+            if 0<=nx<h and 0<=ny<w and arr[nx][ny]==1 and not visited[nx][ny]:
+                dfs(nx,ny)
 while True:
     cnt=0
     w,h=map(int,input().split())
     if w==0 and h==0:
         break
+    visited=[[0]*w for _ in range(h)]
     arr=[list(map(int,input().split())) for _ in range(h)]
     for i in range(h):
         for j in range(w):
-            if arr[i][j]==1:
-                bfs(i,j)
+            if arr[i][j]==1 and not visited[i][j]:
+                dfs(i,j)
                 cnt+=1
     print(cnt)
