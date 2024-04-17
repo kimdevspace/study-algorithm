0552dcf8206176feee11362a2ccb3707d35c7e33
Eui Joong Kim <158041455+kimdevspace@users.noreply.github.com>
2024-04-18 00:21:21 +0900

    [Silver I] Title: 전쟁 - 전투, Time: 52 ms, Memory: 31132 KB -BaekjoonHub

--- 백준/Silver/1303. 전쟁 － 전투/전쟁 － 전투.py
+++ 백준/Silver/1303. 전쟁 － 전투/전쟁 － 전투.py
@@ -1,4 +1,6 @@
-from collections import deque
+import sys
+sys.setrecursionlimit(10**6)
+input=sys.stdin.readline
 n,m=map(int,input().split())
 graph=[list(input()) for _ in range(m)]
 visited=[[False]*n for _ in range(m)]
@@ -6,23 +8,19 @@ ans=[0,0]
 dx=[-1,1,0,0]
 dy=[0,0,-1,1]
 
-def bfs(x,y):
-    cnt=0
-    q=deque([(x,y)])
+def dfs(x,y):
     visited[x][y]=True
-    while q:
-        x,y=q.popleft()
-        for i in range(4):
-            nx,ny=x+dx[i],y+dy[i]
-            if 0<=nx<m and 0<=ny<n and not visited[nx][ny] and graph[x][y]==graph[nx][ny]:
-                q.append((nx,ny))
-                visited[nx][ny]=True
-                cnt+=1
-    return cnt+1
+    cnt=1
+    for i in range(4):
+        nx,ny=x+dx[i],y+dy[i]
+        if 0<=nx<m and 0<=ny<n and not visited[nx][ny] and graph[x][y]==graph[nx][ny]:
+            cnt+=dfs(nx,ny)
+    return cnt
+
 for i in range(m):
     for j in range(n):
         if graph[i][j]=='W' and not visited[i][j]:
-            ans[0]+=bfs(i,j)**2
+            ans[0]+=dfs(i,j)**2
         elif graph[i][j]=='B' and not visited[i][j]:
-            ans[1]+=bfs(i,j)**2
+            ans[1]+=dfs(i,j)**2
 print(*ans)
