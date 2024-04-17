2978870cd3d214b92463f1879d909c719c8c16b6
Eui Joong Kim <158041455+kimdevspace@users.noreply.github.com>
2024-04-18 01:30:50 +0900

    [Silver I] Title: 양치기 꿍, Time: 108 ms, Memory: 34088 KB -BaekjoonHub

---
+++ 백준/Silver/3187. 양치기 꿍/양치기 꿍.py
@@ -0,0 +1,40 @@
+from collections import deque
+r,c=map(int,input().split())
+graph=[list(input()) for _ in range(r)]
+dx=[-1,1,0,0]
+dy=[0,0,-1,1]
+
+
+def bfs(x,y):
+    sheep=0
+    wolf=0
+    q=deque([(x,y)])
+    if graph[x][y]=='k':
+        sheep+=1
+    elif graph[x][y]=='v':
+        wolf+=1
+    graph[x][y]='#'
+    while q:
+        x,y=q.popleft()
+        for i in range(4):
+            nx,ny=x+dx[i],y+dy[i]
+            if 0<=nx<r and 0<=ny<c and graph[nx][ny]!='#':
+                if graph[nx][ny]=='k':
+                    sheep+=1
+                elif graph[nx][ny]=='v':
+                    wolf+=1
+                q.append((nx,ny))
+                graph[nx][ny]='#'
+    if sheep>wolf:
+        return sheep,0
+    elif wolf>=sheep:
+        return 0,wolf
+
+s,w=0,0                
+for i in range(r):
+    for j in range(c):
+        if graph[i][j] in 'kv':
+            ts,tw=bfs(i,j)
+            s+=ts
+            w+=tw
+print(s,w)
