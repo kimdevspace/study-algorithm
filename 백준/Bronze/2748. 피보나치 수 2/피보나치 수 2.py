7ee11a35d56ba2dca4bf2c89c86c5b6ebbc4223d
Eui Joong Kim <158041455+kimdevspace@users.noreply.github.com>
2024-04-25 00:00:10 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -1,9 +1,7 @@
-dp=[0]*91
-def fibo(x):
-    if x==1 or x==2:
-        return 1
-    if dp[x]!=0:
-        return dp[x]
-    dp[x]=fibo(x-1)+fibo(x-2)
-    return dp[x]
-print(fibo(int(input())))
+d=[0]*91
+d[1]=1
+d[2]=1
+n=int(input())
+for i in range(3,n+1):
+    d[i]=d[i-1]+d[i-2]
+print(d[n])
