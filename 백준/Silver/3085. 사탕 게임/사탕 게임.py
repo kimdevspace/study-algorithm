23dc3c28246aea181a964b1364f1c3e564763246
Eui Joong Kim <158041455+kimdevspace@users.noreply.github.com>
2024-05-01 23:41:00 +0900

    [Silver II] Title: 사탕 게임, Time: 3168 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.py
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.py
@@ -1,35 +1,36 @@
+#상하좌우 할 필요 없음 중복되기 떄문. 따라서 오른쪽, 밑쪽만 바꾸면 됨
+import sys
+input=sys.stdin.readline
 n=int(input())
 arr=[list(input()) for _ in range(n)]
-ans=int(-1e9)
 
+max_cnt=0
 def check():
-    max_cnt=1
+    global max_cnt
     for i in range(n):
-        cnt=1
+        cnt=1 #행을 검사
         for j in range(1,n):
             if arr[i][j]==arr[i][j-1]:
                 cnt+=1
+                max_cnt=max(max_cnt,cnt)
             else:
                 cnt=1
-            max_cnt=max(cnt,max_cnt)
-        
-        cnt=1
+        cnt=1 #열을 검사
         for j in range(1,n):
             if arr[j][i]==arr[j-1][i]:
                 cnt+=1
+                max_cnt=max(max_cnt,cnt)
             else:
                 cnt=1
-            max_cnt=max(cnt,max_cnt)
-    return max_cnt
-
+                
 for i in range(n):
-    for j in range(n-1):
-        if j+1<n and arr[i][j]!=arr[i][j+1]:
+    for j in range(n):
+        if j+1<n: #오른쪽과 바꿈
             arr[i][j],arr[i][j+1]=arr[i][j+1],arr[i][j]
-            ans=max(ans,check())
+            check()
             arr[i][j],arr[i][j+1]=arr[i][j+1],arr[i][j]
-        if i+1<n and arr[i][j]!=arr[i+1][j]:
+        if i+1<n: #아래쪽과 바꿈
             arr[i][j],arr[i+1][j]=arr[i+1][j],arr[i][j]
-            ans=max(ans,check())
+            check()
             arr[i][j],arr[i+1][j]=arr[i+1][j],arr[i][j]
-print(ans)
+print(max_cnt)
