20341a59c1d2b9ea4488263178db0a2cccf05032
Eui Joong Kim <158041455+kimdevspace@users.noreply.github.com>
2024-05-03 01:27:35 +0900

    [Silver III] Title: 숫자 야구, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2503. 숫자 야구/숫자 야구.py
+++ 백준/Silver/2503. 숫자 야구/숫자 야구.py
@@ -1,31 +1,28 @@
-from sys import stdin
+import sys
+input=sys.stdin.readline
 
-N=int(stdin.readline())
-cur=[]
-for x in range(100,1000):
-    strx=str(x)
-    if strx[0]!=strx[1] and strx[1]!=strx[2] and strx[0] != strx[2]:
-        if strx[0]!='0' and strx[1] !='0' and strx[2] !='0':
-            cur.append(x)
-answer=[]
+nums=[]
+for i in range(123,988):
+    stri=str(i)
+    if stri[0]!=stri[1] and stri[0]!=stri[2] and stri[1]!=stri[2]:
+        if stri[0]!='0' and stri[1]!='0' and stri[2]!='0':
+            nums.append(stri)
 
-for x in range(N):
-    num,s,b = map(int,stdin.readline().split())
+n=int(input())
+for _ in range(n):
+    num,s,b=map(int,input().split())
     str_num=str(num)
-    for bf_num in cur:
-        str_bf_num=str(bf_num)
-        tmps=0
-        tmpb=0
-        for y in range(3):
-            for z in range(3):
-                if str_num[y] == str_bf_num[z]:
-                    if y==z:
+    tmp=[]
+    for number in nums:
+        tmps,tmpb=0,0
+        for i in range(3):
+            for j in range(3):
+                if str_num[i]==number[j]:
+                    if i==j:
                         tmps+=1
                         break
                     tmpb+=1
-        if tmps == s and tmpb == b:
-            answer.append(bf_num)        
-    cur = answer
-    answer=[] 
-        
-print(len(cur))
+        if tmps==s and tmpb==b:
+            tmp.append(number)
+    nums=tmp
+print(len(nums))
