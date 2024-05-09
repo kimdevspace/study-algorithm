e3185d8cdaa919410b8e1a54cf5034c30b0c960a
김의중 <158041455+kimdevspace@users.noreply.github.com>
2024-05-09 15:42:10 +0900

    [Silver I] Title: 후보 추천하기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1713. 후보 추천하기/후보 추천하기.py
@@ -0,0 +1,19 @@
+n=int(input())
+stud=int(input())
+arr=list(map(int,input().split()))
+picture=[]
+cnt=[]
+
+for a in arr:
+    if a in picture:
+        cnt[picture.index(a)]+=1
+    else:
+        if len(picture)>=n:
+            idx=cnt.index(min(cnt))
+            del picture[idx]
+            del cnt[idx]
+        picture.append(a)
+        cnt.append(1)
+
+picture.sort()
+print(*picture)
