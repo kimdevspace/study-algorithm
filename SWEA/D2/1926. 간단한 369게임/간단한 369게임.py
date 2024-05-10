8a42b7e3241d0929e6d892e6fdf1e3d26328263d
김의중 <158041455+kimdevspace@users.noreply.github.com>
2024-05-10 17:49:05 +0900

    [D2] Title: 간단한 369게임, Time: 97 ms, Memory: 44,728 KB -BaekjoonHub

--- SWEA/D2/1926. 간단한 369게임/간단한 369게임.py
+++ SWEA/D2/1926. 간단한 369게임/간단한 369게임.py
@@ -1,10 +1,10 @@
 n=int(input())
 for i in range(1,n+1):
-    cnt=0 #박수횟수
+    clap=0
     for j in str(i):
         if j in ['3','6','9']:
-            cnt+=1
-    if cnt==0:
+            clap+=1
+    if clap==0:
         print(i,end=' ')
     else:
-        print('-'*cnt,end=' ')
+        print('-'*clap,end=' ')
