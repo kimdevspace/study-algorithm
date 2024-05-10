d9fb6432a23b2052ccc215b667ccd02097b097f9
김의중 <158041455+kimdevspace@users.noreply.github.com>
2024-05-10 16:32:54 +0900

    [Silver III] Title: 킹, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1063. 킹/킹.py
+++ 백준/Silver/1063. 킹/킹.py
@@ -1,27 +1,21 @@
-move={'R':[1,0],'L':[-1,0],'B':[0,-1],'T':[0,1]
-      ,'RT':[1,1],'LT':[-1,1],'RB':[1, -1],
-      'LB':[-1,-1]}
+dic={'R':[1,0],'L':[-1,0],'B':[0,-1],'T':[0,1]
+     ,'RT':[1,1],'LT':[-1,1],'RB':[1,-1],'LB':[-1,-1]}
+king, stone, n = input().split()
 
-king,rock,n=input().split()
-n=int(n)
+k=[ord(king[0])-65, int(king[1])-1]
+s=[ord(stone[0])-65, int(stone[1])-1]
 
-k=[ord(king[0])-65,int(king[1])-1]
-r=[ord(rock[0])-65,int(rock[1])-1]
-
-for _ in range(n):
-    m=input()
-    nx=k[0]+move[m][0]
-    ny=k[1]+move[m][1]
-
+for _ in range(int(n)):
+    move=input()
+    nx,ny=k[0]+dic[move][0],k[1]+dic[move][1]
     if 0<=nx<8 and 0<=ny<8:
-        if nx==r[0] and ny==r[1]:
-            rx=r[0]+move[m][0]
-            ry=r[1]+move[m][1]
-            if 0<=rx<8 and 0<=ry<8:
+        if nx==s[0] and ny==s[1]:
+            sx=s[0]+dic[move][0]
+            sy=s[1]+dic[move][1]
+            if 0<=sx<8 and 0<=sy<8:
                 k=[nx,ny]
-                r=[rx,ry]
+                s=[sx,sy]
         else:
             k=[nx,ny]
-            
 print(str(chr(k[0]+65))+str(k[1]+1))
-print(str(chr(r[0]+65))+str(r[1]+1))
+print(str(chr(s[0]+65))+str(s[1]+1))
