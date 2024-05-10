053a386877a6b19e0c9a5dd3bac89170dc656080
김의중 <158041455+kimdevspace@users.noreply.github.com>
2024-05-10 17:44:33 +0900

    [D2] Title: 백만 장자 프로젝트, Time: 1,147 ms, Memory: 185,424 KB -BaekjoonHub

--- SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.py
+++ SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.py
@@ -1,13 +1,12 @@
-T = int(input())
-
-for test_case in range(1, T + 1):
+t=int(input())
+for tc in range(1,t+1):
     n=int(input())
-    ans=0
     arr=list(map(int,input().split()))
-    sellPrice=0
+    sell=0
+    ans=0
     for a in arr[::-1]:
-        if a>=sellPrice:
-            sellPrice=a
+        if a>sell:
+            sell=a
         else:
-            ans+=sellPrice-a
-    print('#'+str(test_case),ans)
+            ans+=sell-a
+    print('#'+str(tc),ans)
