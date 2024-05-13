f81d01d0d52efa0b9cc5525bcc9b4006934892de
김의중 <158041455+kimdevspace@users.noreply.github.com>
2024-05-13 12:11:23 +0900

    [D3] Title: 식료품 가게, Time: 165 ms, Memory: 49,764 KB -BaekjoonHub

--- SWEA/D3/19113. 식료품 가게/식료품 가게.py
+++ SWEA/D3/19113. 식료품 가게/식료품 가게.py
@@ -4,9 +4,9 @@ for tc in range(1,t+1):
     arr=list(map(int,input().split()))
     ans=[]
     for i in range(len(arr)):
-        w=arr[i]//0.75
-        if w%4==0 and w in arr:
+        tmp=arr[i]//0.75
+        if tmp%4==0 and tmp in arr:
             ans.append(arr[i])
-            arr[arr.index(w)]-=1
+            arr[arr.index(tmp)]-=1
     ans.sort()
     print('#'+str(tc),*ans)
