76ed2e1ffa9019126e87c9650e9f1033b60a3239
김의중 <158041455+kimdevspace@users.noreply.github.com>
2024-05-17 23:28:31 +0900

    [D3] Title: 진기의 최고급 붕어빵, Time: 341 ms, Memory: 53,184 KB -BaekjoonHub

--- SWEA/D3/1860. 진기의 최고급 붕어빵/진기의 최고급 붕어빵.py
+++ SWEA/D3/1860. 진기의 최고급 붕어빵/진기의 최고급 붕어빵.py
@@ -3,11 +3,9 @@ for tc in range(1,t+1):
     n,m,k=map(int,input().split())
     lst=list(map(int,input().split()))
     lst.sort()
-    ans='Possible'
-    cnt=0
-    for l in lst:
-        cnt+=1
-        if (l//m)*k < cnt:
-            ans='Impossible'
+    res='Possible'
+    for i in range(n):
+        if 0 > (lst[i]//m)*k-(i+1):
+            res='Impossible'
             break
-    print('#'+str(tc),ans)
+    print('#'+str(tc),res)
