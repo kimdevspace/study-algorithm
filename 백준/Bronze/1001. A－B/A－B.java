46148036f710c64c2a1509a230799156dd13a61b
김의중 <158041455+kimdevspace@users.noreply.github.com>
2024-05-29 02:15:02 +0900

    [Bronze V] Title: A-B, Time: 120 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Bronze/1001. A－B/A－B.java
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -1,12 +1,13 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String str = br.readLine();
-        StringTokenizer st = new StringTokenizer(str," ");
+        StringTokenizer st = new StringTokenizer(str, " ");
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
 
