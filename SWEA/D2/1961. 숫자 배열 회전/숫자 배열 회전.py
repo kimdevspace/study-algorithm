63a6f25faef4a64c675c212b53677ebd039b0b43
김의중 <158041455+kimdevspace@users.noreply.github.com>
2024-07-01 17:33:31 +0900

    [D2] Title: 숫자 배열 회전, Time: 96 ms, Memory: 44,728 KB -BaekjoonHub

--- SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.py
+++ SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.py
@@ -1,13 +1,64 @@
-t=int(input())
-for tc in range(1,t+1):
-    n=int(input())
-    arr=[list(map(int,input().split())) for _ in range(n)]
-    arr_90=list(map(list,zip(*arr[::-1])))
-    arr_180=list(map(list,zip(*arr_90[::-1])))
-    arr_270=list(map(list,zip(*arr_180[::-1])))
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-    print('#'+str(tc))
-    for i in range(len(arr)):
-        print(''.join(map(str,arr_90[i])),end=' ')
-        print(''.join(map(str,arr_180[i])),end=' ')
-        print(''.join(map(str,arr_270[i])))
+class Solution {
+    //90도 회전 함수
+    public static int[][] rotate(int arr[][]) {
+        int len = arr.length;
+        int res[][] = new int[len][len];
+
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len; j++) {
+                res[i][j] = arr[len - 1 - j][i];
+            }
+        }
+        return res;
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int tc = 1; tc <= T; tc++) {
+            int N = Integer.parseInt(br.readLine());
+            int[][] arr = new int[N][N];
+
+            StringTokenizer st;
+            for (int i = 0; i < N; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < N; j++) {
+                    arr[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+            int arr90[][] = new int[N][N];
+            int arr180[][] = new int[N][N];
+            int arr270[][] = new int[N][N];
+
+            arr90 = rotate(arr);
+            arr180 = rotate(arr90);
+            arr270 = rotate(arr180);
+
+            bw.write("#" + tc + "\n");
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    bw.write(arr90[i][j]+"");
+                }
+                bw.write(" ");
+                for (int j = 0; j < N; j++) {
+                    bw.write(arr180[i][j]+"");
+                }
+                bw.write(" ");
+                for (int j = 0; j < N; j++) {
+                    bw.write(arr270[i][j]+"");
+                }
+                bw.write("\n");
+            }
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+
+    }
+}
