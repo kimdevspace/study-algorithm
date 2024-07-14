83e5f31e1eef0685615702fb0d485ec374856b92
김의중 <158041455+kimdevspace@users.noreply.github.com>
2024-07-14 22:39:14 +0900

    [Bronze II] Title: 음계, Time: 172 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[8];
+        for (int i = 0; i < 8; i++) {
+            arr[i] = sc.nextInt();
+        }
+        sc.close();
+
+        String res = "";
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] == arr[i + 1] - 1) res = "ascending";
+            else if (arr[i] == arr[i + 1] + 1) res = "descending";
+            else {
+                res = "mixed";
+                break;
+            }
+        }
+        System.out.println(res);
+    }
+}
