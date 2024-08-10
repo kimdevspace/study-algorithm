81fbfbbc1657c3ec02717eeb971040ff51d67210
kimdevspace <158041455+kimdevspace@users.noreply.github.com>
2024-08-10 14:08:24 +0900

    [Gold III] Title: 게리맨더링, Time: 132 ms, Memory: 15596 KB -BaekjoonHub

--- 백준/Gold/17471. 게리맨더링/게리맨더링.java
+++ 백준/Gold/17471. 게리맨더링/게리맨더링.java
@@ -1,17 +1,19 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
     static int N;
-    static StringTokenizer st;
     static int[] people;
+    static StringTokenizer st;
     static ArrayList<ArrayList<Integer>> adjList;
     static int ans = Integer.MAX_VALUE;
-    static boolean isDiv;
+    static boolean divChk;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         N = Integer.parseInt(br.readLine());
 
         people = new int[N + 1];
@@ -20,76 +22,79 @@ public class Main {
             people[i] = Integer.parseInt(st.nextToken());
         }
 
-
         adjList = new ArrayList<>();
         adjList.add(new ArrayList<>());
-        for (int i = 1; i <= N; i++) {
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            adjList.add(new ArrayList<>());
-            int areaCnt = Integer.parseInt(st.nextToken());
-            for (int j = 0; j < areaCnt; j++) {
-                int element = Integer.parseInt(st.nextToken());
-                adjList.get(i).add(element);
+            int adjCnt = Integer.parseInt(st.nextToken());
+            ArrayList<Integer> adj = new ArrayList<>();
+            for (int j = 0; j < adjCnt; j++) {
+                adj.add(Integer.parseInt(st.nextToken()));
             }
+            adjList.add(adj);
         }
+
+        //구역 나누기
         for (int i = 1; i <= N/2; i++) {
             boolean[] visited = new boolean[N + 1];
             combine(1, i, new ArrayList<>(), visited);
         }
-        if (isDiv) System.out.println(ans);
-        else System.out.println(-1);
 
+        if (divChk) {
+            bw.write(ans + "");
+        }
+        else {
+            bw.write(-1 + "");
+        }
+        br.close();
+        bw.flush();
+        bw.close();
     }
-    public static void combine(int start, int maxLen, ArrayList<Integer> groupA, boolean[] visited) {
+    public static void combine(int start, int end, ArrayList<Integer> groupA, boolean[] visited) {
 
-        if (groupA.size() == maxLen) {
+        if (groupA.size() == end) {
             ArrayList<Integer> groupB = new ArrayList<>();
             for (int i = 1; i <= N; i++) {
                 if (!visited[i]) {
                     groupB.add(i);
                 }
             }
-
-
-            if (isConnected(groupA) && isConnected(groupB)) {
-                int A_sum = 0;
-                int B_sum = 0;
+            if (isLinked(groupA) && isLinked(groupB)) {
+                int sum_A = 0, sum_B = 0;
                 for (int i = 0; i < groupA.size(); i++) {
-                    A_sum += people[groupA.get(i)];
+                    sum_A += people[groupA.get(i)];
                 }
                 for (int i = 0; i < groupB.size(); i++) {
-                    B_sum += people[groupB.get(i)];
+                    sum_B += people[groupB.get(i)];
                 }
-                ans = Math.min(ans, Math.abs(A_sum - B_sum));
-                isDiv = true;
+                ans = Math.min(ans, Math.abs(sum_A - sum_B));
+                divChk = true;
             }
             return;
         }
 
-
         for (int i = start; i <= N; i++) {
             visited[i] = true;
             groupA.add(i);
-            combine(i + 1, maxLen, groupA, visited);
+            combine(i + 1, end, groupA, visited);
             groupA.remove(groupA.size() - 1);
-            visited[i]= false;
+            visited[i] = false;
         }
     }
-
-    public static boolean isConnected(ArrayList<Integer> group) {
+    public static boolean isLinked(ArrayList<Integer> group) {
         boolean[] visited = new boolean[N + 1];
         Queue<Integer> q = new LinkedList<>();
-        q.offer(group.get(0));
+        q.add(group.get(0));
         visited[group.get(0)] = true;
         int cnt = 1;
 
         while (!q.isEmpty()) {
             int cur = q.poll();
-            for (int neighbor : adjList.get(cur)) {
-                if (group.contains(neighbor) && !visited[neighbor]) {
-                    visited[neighbor] = true;
-                    q.offer(neighbor);
+            for (int link : adjList.get(cur)) {
+                if (!visited[link] && group.contains(link)) {
                     cnt++;
+                    q.add(link);
+                    visited[link] = true;
                 }
             }
         }
