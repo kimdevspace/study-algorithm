fe9894b90e5c9da2ad091776be3d43d38ac2cb5e
kimdevspace <158041455+kimdevspace@users.noreply.github.com>
2024-08-28 23:02:17 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 940 ms, Memory: 172588 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -2,11 +2,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    private static int[] arr;
+    private static HashMap<Integer, Integer> hashMap;
     private static int N, M;
 
     public static void main(String[] args) throws IOException {
@@ -14,49 +14,22 @@ public class Main {
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
 
+        hashMap = new HashMap<>();
         N = Integer.parseInt(br.readLine());
-        arr = new int[N];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            int key = Integer.parseInt(st.nextToken());
+            hashMap.put(key, hashMap.getOrDefault(key, 0) + 1);
         }
-        Arrays.sort(arr);
+
+
         M = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < M; i++) {
             int target = Integer.parseInt(st.nextToken());
-            sb.append(findHighIndex(target)-findLowIndex(target)).append(" ");
+            sb.append(hashMap.getOrDefault(target, 0)).append(" ");
         }
         System.out.println(sb);
     }
-
-    private static int findLowIndex(int target) {
-        int start = 0;
-        int end = N;
-
-        while (start < end) {
-            int mid = (start + end) / 2;
-            if (arr[mid] >= target) {
-                end = mid;
-            }else {
-                start = mid + 1;
-            }
-        }
-        return start;
-    }
-
-    private static int findHighIndex(int target) {
-        int start = 0;
-        int end = N;
-
-        while (start < end) {
-            int mid = (start + end) / 2;
-            if (arr[mid] <= target) {
-                start = mid + 1;
-            }else {
-                end = mid;
-            }
-        }
-        return start;
-    }
+    
 }
