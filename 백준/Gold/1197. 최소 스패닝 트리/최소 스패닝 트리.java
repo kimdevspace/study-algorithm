7e84b996819a3922346448a6172ffb351cf85408
kimdevspace <158041455+kimdevspace@users.noreply.github.com>
2024-08-29 10:22:27 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 584 ms, Memory: 49764 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static int V; //컴퓨터의 수
+    private static int E; //최대 간선의 수
+    private static int[] p; //서로소 집합
+    private static int[][] graph; //연결 정보를 담을 거
+    private static int ans;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        V = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+
+        graph = new int[E][3];
+
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 3; j++) {
+                graph[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        //가중치 기준으로 오름차순 정렬
+        Arrays.sort(graph, (o1, o2) -> o1[2] - o2[2]);
+        makeSet();
+        System.out.println(kruskal());
+
+
+    }
+    private static void makeSet() {
+        p = new int[V + 1];
+        for (int i = 1; i <= V; i++) {
+            p[i] = i;
+        }
+    }
+
+    private static int find(int x) {
+        if (x == p[x]) {
+            return x;
+        }
+        return p[x] = find(p[x]);
+    }
+
+    private static void union(int x, int y) {
+        x = find(x);
+        y = find(y);
+        if (x < y) {
+            p[y] = x;
+        }else {
+            p[x] = y;
+        }
+    }
+
+    private static int kruskal() {
+        int cost = 0;
+        for (int i = 0; i < graph.length; i++) {
+            if (find(graph[i][0]) != find(graph[i][1])) {
+                cost += graph[i][2];
+                union(find(graph[i][0]), find(graph[i][1]));
+            }
+        }
+        return cost;
+    }
+}
