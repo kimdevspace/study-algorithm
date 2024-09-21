fa51d481050b80dcfb6e9a3d0710f22c1eb8a231
kimdevspace <158041455+kimdevspace@users.noreply.github.com>
2024-09-21 23:49:00 +0900

    [Bronze IV] Title: 기찍 N, Time: 812 ms, Memory: 33380 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        for (int i = sc.nextInt(); i >= 1; i--) {
+            System.out.println(i);
+        }
+    }
+}
