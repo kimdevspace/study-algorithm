3691d841d073ac7257bbd82b9f2ecc8101ff4af5
kimdevspace <158041455+kimdevspace@users.noreply.github.com>
2024-10-05 14:45:59 +0900

    [Gold III] Title: 마법사 상어와 토네이도, Time: 440 ms, Memory: 34036 KB -BaekjoonHub

---
+++ 백준/Gold/20057. 마법사 상어와 토네이도/마법사 상어와 토네이도.java
@@ -0,0 +1,143 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N;
+    static int[][] map;
+    static int ans;
+
+    //좌, 하, 우, 상
+    static int[][] dr = {
+            {0, -1, 1, -1, 1, -2, 2, -1, 1},
+            {2, 1, 1, 0, 0, 0, 0, -1, -1},
+            {0, -1, 1, -1, 1, -2, 2, -1, 1},
+            {-2, -1, -1, 0, 0, 0, 0, 1, 1}
+    };
+    static int[][] dc = {
+            {-2, -1, -1, 0, 0, 0, 0, 1, 1},
+            {0, -1, 1, -1, 1, -2, 2, -1, 1},
+            {2, 1, 1, 0, 0, 0, 0, -1, -1},
+            {0, -1, 1, -1, 1, -2, 2, -1, 1}
+    };
+    static int[] ratio = {5, 10, 10, 7, 7, 2, 2, 1, 1};
+    static int[][] push = {
+            {0, 1, 0, -1},
+            {-1, 0, 1, 0}
+    };
+
+    public static void main(String[] args) throws IOException {
+        input();
+        tornado();
+        System.out.println(ans);
+    }
+
+    private static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        N = Integer.parseInt(br.readLine());
+        map = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+    }
+
+    private static void tornado() {
+        int num = 1;
+        int r = N / 2;
+        int c = N / 2;
+        int step = 1;
+        int dir = 0;
+
+        while (num < N * N) {
+
+            for (int d = 0; d < 2; d++) {
+                for (int i = 0; i < step; i++) {
+                    if (num >= N * N) break;
+
+                    switch (dir) {
+                        case 0: c--; break; //좌
+                        case 1: r++; break; //하
+                        case 2: c++; break; //우
+                        case 3: r--; break; //상
+                    }
+                    num++;
+                    if (map[r][c] != 0) {
+                        spread(r, c, dir);
+                    }
+                }
+                dir = (dir + 1 ) % 4;
+            }
+            step++;
+        }
+    }
+
+    private static void spread(int r, int c, int dir) {
+        /*
+        int[][] tmpMap = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (i == r && j ==c ) continue;
+                tmpMap[i][j] = map[i][j];
+            }
+        }
+         */
+
+        int sand = map[r][c];
+        int totalSpread = 0;
+
+
+        for (int i = 0; i < 9; i++) {
+            int nr = r + dr[dir][i];
+            int nc = c + dc[dir][i];
+            int spreadAmount = sand * ratio[i] / 100;
+            totalSpread += spreadAmount;
+
+            if (isRange(nr, nc)) {
+                map[nr][nc] += spreadAmount;
+            }else {
+                ans += spreadAmount;
+            }
+        }
+
+        int push_r = r + push[0][dir], push_c = c + push[1][dir];
+        int remainingSand = sand - totalSpread;
+        if (isRange(push_r, push_c)) {
+            map[push_r][push_c] += remainingSand;
+        }else {
+            ans += remainingSand;
+        }
+
+        //print(tmpMap);
+        //System.out.println();
+        //copyMap(tmpMap);
+    }
+
+    private static boolean isRange(int nr, int nc) {
+        if (0 <= nr && nr < N && 0 <= nc && nc < N) return true;
+        return false;
+    }
+
+    private static void copyMap(int[][] tmpMap) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                map[i][j] = tmpMap[i][j];
+            }
+        }
+    }
+
+    private static void print(int[][] arr) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
