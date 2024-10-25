cc170d80433c09e22235dc87859eac43e97b728d
kimdevspace <158041455+kimdevspace@users.noreply.github.com>
2024-10-25 10:41:56 +0900

    [Silver V] Title: 덩치, Time: 120 ms, Memory: 15732 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int[][] people = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            people[i][0] = Integer.parseInt(st.nextToken());
+            people[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] rank = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            int cnt = 1;
+            for (int j = 0; j < N; j++) {
+                if (i != j && people[i][0] < people[j][0] && people[i][1] < people[j][1]) {
+                    cnt++;
+                }
+            }
+            rank[i] = cnt;
+        }
+
+        for (int i : rank) {
+            System.out.print(i + " ");
+        }
+    }
+}
