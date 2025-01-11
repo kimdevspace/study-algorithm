1368f5af35049d3b400eb281027ef501b51ee091
kimdevspace <158041455+kimdevspace@users.noreply.github.com>
2025-01-11 17:38:50 +0900

    [Gold V] Title: 퇴사 2, Time: 664 ms, Memory: 315944 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.java
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -11,22 +12,23 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
         int[] time = new int[N + 1];
         int[] pay = new int[N + 1];
-        int[] dp = new int[N + 2];
+        int[] dp = new int[N + 1];
         for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
             time[i] = Integer.parseInt(st.nextToken());
             pay[i] = Integer.parseInt(st.nextToken());
         }
 
+        //System.out.println(Arrays.toString(dp));
         for (int i = 1; i <= N; i++) {
             dp[i] = Math.max(dp[i], dp[i - 1]);
 
             if (i + time[i] - 1 <= N) {
-                dp[i + time[i]] = Math.max(dp[i + time[i]], dp[i] + pay[i]);
+                dp[i + time[i] - 1] = Math.max(dp[i + time[i] - 1], dp[i - 1] + pay[i]);
             }
+            //System.out.println(Arrays.toString(dp));
         }
 
-        dp[N + 1] = Math.max(dp[N + 1], dp[N]);
-        System.out.println(dp[N + 1]);
+        System.out.println(dp[N]);
     }
 }
