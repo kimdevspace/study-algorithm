8e534f90162bceec9e40f6fbfc99241747fca810
kimdevspace <158041455+kimdevspace@users.noreply.github.com>
2025-02-07 22:25:27 +0900

    [Gold III] Title: 앱, Time: 120 ms, Memory: 14488 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.java
+++ 백준/Gold/7579. 앱/앱.java
@@ -4,42 +4,39 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    static int N, M, ans;
-    static int[] memories, costs;
-    static int[][] dp;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        memories = new int[N + 1];
-        costs = new int[N + 1];
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] memories = new int[N + 1];
+        int[] costs = new int[N + 1];
         st = new StringTokenizer(br.readLine());
         for (int i = 1; i < N + 1; i++) {
             memories[i] = Integer.parseInt(st.nextToken());
         }
         st = new StringTokenizer(br.readLine());
+        int maxCost = 0;
         for (int i = 1; i < N + 1; i++) {
             costs[i] = Integer.parseInt(st.nextToken());
+            maxCost += costs[i];
         }
 
-        dp = new int[N + 1][10001];
-        ans = Integer.MAX_VALUE;
+        int[] dp = new int[maxCost + 1];
         for (int i = 1; i < N + 1; i++) {
+            int memory = memories[i];
             int cost = costs[i];
-            for (int j = 0; j < 10001; j++) {
-                if (cost > j) {
-                    dp[i][j] = dp[i - 1][j];
-                } else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - cost] + memories[i]);
-                }
+            for (int j = maxCost; j >= cost; j--) {
+                dp[j] = Math.max(dp[j], dp[j - cost] + memory);
+            }
+        }
 
-                if (dp[i][j] >= M) {
-                    ans = Math.min(ans, j);
-                }
+        int ans = 0;
+        for (int i = 0; i < maxCost + 1; i++) {
+            if (dp[i] >= M) {
+                ans = i;
+                break;
             }
         }
 
