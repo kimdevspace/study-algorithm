3429707685c0e26c0961947b3cf7c23103aa9f57
kimdevspace <158041455+kimdevspace@users.noreply.github.com>
2025-03-26 09:22:09 +0900

    [Silver V] Title: 돌 게임, Time: 104 ms, Memory: 14364 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.java
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -3,11 +3,22 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+
+    static int[] dp = new int[1001];
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        if (N % 2 == 0) System.out.println("CY");
-        else System.out.println("SK");
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 1;
+
+        for (int i = 4; i <= N; i++) {
+            dp[i] = Math.min(dp[i - 1], dp[i - 3]) + 1;
+        }
+
+        System.out.println(dp[N] % 2 == 0 ? "CY" : "SK");
+
     }
 }
