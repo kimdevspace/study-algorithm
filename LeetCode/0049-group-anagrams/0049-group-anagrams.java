7f13108a1aa0971d59714a349cec3a0fb63adc81
kimdevspace <158041455+kimdevspace@users.noreply.github.com>
2025-06-26 20:58:16 +0900

    Time: 6 ms (98.15%), Space: 47.7 MB (90.76%) - LeetHub

---
+++ LeetCode/0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String str : strs) {
+            char[] c = str.toCharArray();
+            Arrays.sort(c);
+            String key = String.valueOf(c);
+
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+
+            map.get(key).add(str);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
