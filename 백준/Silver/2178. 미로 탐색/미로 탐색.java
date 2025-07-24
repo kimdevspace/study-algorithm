5e687e2e9c60ab430c38067152036741141b3f3e
kimdevspace <158041455+kimdevspace@users.noreply.github.com>
2025-07-24 16:43:19 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 14428 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,45 +1,68 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[][] arr;
+
+    static class Point {
+        int r, c;
+
+        public Point(int r, int c) {
+            this.r = r;
+            this.c = c;
+        }
+    }
+
     static int N, M;
+    static int[][] map;
+    static boolean[][] visited;
+    static int[] dr = {-1, 1, 0, 0};
+    static int[] dc = {0, 0, -1, 1};
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        arr = new int[N][M];
+
+        map = new int[N][M];
+        visited = new boolean[N][M];
+
         for (int i = 0; i < N; i++) {
-            String str = br.readLine();
+            String input = br.readLine();
             for (int j = 0; j < M; j++) {
-                arr[i][j] = str.charAt(j) - '0';
+                map[i][j] = input.charAt(j) - '0';
             }
         }
-        bfs();
-        System.out.println(arr[N-1][M-1]);
 
+        bfs(0, 0);
+
+        System.out.println(map[N - 1][M - 1]);
     }
-    public static void bfs() {
-        int[] dr = {0,0,-1,1};
-        int[] dc = {1,-1,0,0};
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[] {0 ,0});
+
+    private static void bfs(int r, int c) {
+        Queue<Point> q = new ArrayDeque<>();
+        q.offer(new Point(r, c));
+        visited[r][c] = true;
 
         while (!q.isEmpty()) {
-            int[] lst = q.poll();
-            int cur_r = lst[0], cur_c = lst[1];
+            Point p = q.poll();
+
+            int cur_r = p.r;
+            int cur_c = p.c;
+
             for (int i = 0; i < 4; i++) {
                 int nr = cur_r + dr[i];
                 int nc = cur_c + dc[i];
-                if (nr >= 0 && nr < N && nc >= 0 && nc < M && arr[nr][nc] == 1) {
-                    arr[nr][nc] = arr[cur_r][cur_c] + 1;
-                    q.offer(new int[] {nr,nc});
+
+                if (0 <= nr && nr < N && 0 <= nc && nc < M && !visited[nr][nc] && map[nr][nc] != 0) {
+                    q.offer(new Point(nr, nc));
+                    visited[nr][nc] = true;
+                    map[nr][nc] = map[cur_r][cur_c] + 1;
                 }
             }
         }
