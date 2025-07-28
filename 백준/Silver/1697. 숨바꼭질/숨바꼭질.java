a764bba49570dbbaf5e8bfe77febb59b126859fe
kimdevspace <158041455+kimdevspace@users.noreply.github.com>
2025-07-29 00:01:45 +0900

    [Silver I] Title: 숨바꼭질, Time: 144 ms, Memory: 20724 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,57 +1,44 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    private static int N, K;
-    private static StringTokenizer st;
-    private static int ans = Integer.MAX_VALUE;
+    static int N, K;
+    static Queue<Integer> q;
+    static int[] visited;
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
 
-        bfs(N, 0);
-        System.out.println(ans);
+        q = new ArrayDeque<>();
+        visited = new int[100001];
 
-    }
-    public static void bfs(int x, int sec) {
-        boolean[] visit = new boolean[100001];
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[] {x, sec});
-        visit[x] = true;
+        q.offer(N);
+        visited[N] = 1;
 
         while (!q.isEmpty()) {
-            int[] lst = q.poll();
-            int cur = lst[0];
-            int depth = lst[1];
+            int cur = q.poll();
 
             if (cur == K) {
-                ans = Math.min(ans, depth);
+                System.out.println(visited[cur] - 1);
                 break;
             }
+            
+            int[] next = {cur - 1, cur + 1, cur * 2};
 
-            if ((cur - 1) >= 0 && !visit[cur - 1]) {
-                q.add(new int[] {cur - 1, depth + 1});
-                visit[cur - 1] = true;
-            }
-
-            if ((cur + 1) < 100001 && !visit[cur + 1]) {
-                q.add(new int[] {cur + 1, depth + 1});
-                visit[cur + 1] = true;
-            }
-
-            if ((cur * 2) < 100001 && !visit[cur * 2]) {
-                q.add(new int[] {cur * 2, depth + 1});
-                visit[cur * 2] = true;
+            for (int nextPos : next) {
+                if (nextPos >= 0 && nextPos < 100001 && visited[nextPos] == 0) {
+                    q.offer(nextPos);
+                    visited[nextPos] = visited[cur] + 1;
+                }
             }
         }
 
