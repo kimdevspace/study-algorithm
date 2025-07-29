4fdbf483e4a3e3ff10f2087101df5d196e1d2d93
kimdevspace <158041455+kimdevspace@users.noreply.github.com>
2025-07-29 22:48:36 +0900

    [Silver I] Title: 안전 영역, Time: 264 ms, Memory: 37012 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -1,60 +1,90 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N;
-    static int[] dr = {0, 0, -1, 1};
-    static int[] dc = {-1, 1, 0, 0};
-    static int[][] graph;
-    static StringTokenizer st;
-    static int ans;
-    static boolean[][] visit;
+
+    static class Point {
+        int r, c;
+
+        public Point(int r, int c) {
+            this.r = r;
+            this.c = c;
+        }
+
+        @Override
+        public String toString() {
+            return "Point{" +
+                    "r=" + r +
+                    ", c=" + c +
+                    '}';
+        }
+    }
+
+    static int N, maxHeight, ans;
+    static int[][] map;
+    static boolean[][] visited;
+    static Queue<Point> q;
+    static int[] dr = {-1, 1, 0, 0};
+    static int[] dc = {0, 0, -1, 1};
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
         N = Integer.parseInt(br.readLine());
-        graph = new int[N][N];
-        int t_max = 0;
+        map = new int[N][N];
+        maxHeight = 0;
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
-                graph[i][j] = Integer.parseInt(st.nextToken());
-                t_max = Math.max(t_max, graph[i][j]);
+                map[i][j] = Integer.parseInt(st.nextToken());
+                maxHeight = Math.max(maxHeight, map[i][j]);
             }
         }
-        for (int t = 0; t < t_max; t++) {
-            visit = new boolean[N][N];
-            int cnt = 0;
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    if (graph[i][j] > t && !visit[i][j]) {
-                        bfs(i, j, t);
-                        cnt++;
+
+        ans = 0;
+
+        for (int i = 0; i <= maxHeight; i++) {
+
+            q = new ArrayDeque<>();
+            visited = new boolean[N][N];
+            int max = 0;
+
+            for (int r = 0; r < N; r++) {
+                for (int c = 0; c < N; c++) {
+                    if (map[r][c] > i && !visited[r][c]) {
+                        bfs(r, c, i);
+                        max++;
                     }
                 }
             }
-            ans = Math.max(ans, cnt);
+            ans = Math.max(ans, max);
         }
-        System.out.println(ans);
 
+        System.out.println(ans);
     }
-    public static void bfs(int r, int c, int height) {
-        Queue<int[]> q = new LinkedList<>();
-        visit[r][c] = true;
-        q.offer(new int[] {r, c});
+
+    private static void bfs(int r, int c, int rainHeight) {
+        q.offer(new Point(r, c));
+        visited[r][c] = true;
 
         while (!q.isEmpty()) {
-            int[] lst = q.poll();
-            int cur_r = lst[0];
-            int cur_c = lst[1];
+            Point point = q.poll();
+
+            int cur_r = point.r;
+            int cur_c = point.c;
+
             for (int i = 0; i < 4; i++) {
-                int nr = cur_r + dr[i], nc = cur_c + dc[i];
-                if (nr >= 0 && nr < N && nc >= 0 && nc < N && !visit[nr][nc] && graph[nr][nc] > height) {
-                    q.offer(new int[] {nr, nc});
-                    visit[nr][nc] = true;
+                int nr = cur_r + dr[i];
+                int nc = cur_c + dc[i];
+
+                if (0 <= nr && nr < N && 0 <= nc && nc < N && !visited[nr][nc] && map[nr][nc] > rainHeight) {
+                    q.offer(new Point(nr, nc));
+                    visited[nr][nc] = true;
                 }
             }
         }
