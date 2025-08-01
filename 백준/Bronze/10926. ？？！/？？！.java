cccaad9f5f7ba8f2d6f43a38d717649ca2fd4fdb
kimdevspace <158041455+kimdevspace@users.noreply.github.com>
2025-08-01 17:42:21 +0900

    [Bronze V] Title: ??!, Time: 172 ms, Memory: 17680 KB -BaekjoonHub

--- 백준/Bronze/10926. ？？！/？？！.java
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -1,13 +1,9 @@
-import java.io.*;
+import java.util.*;
 
-public class Main{
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String a = "??!";
-        String s = br.readLine();
-        bw.write(s+a);
-        bw.flush();
-        bw.close();
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        System.out.println(s + "??!");
     }
 }
