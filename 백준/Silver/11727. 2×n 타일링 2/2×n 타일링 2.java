c23b1cfab16846acbe2740a7a4425d6c5bb8782a
kimdevspace <158041455+kimdevspace@users.noreply.github.com>
2025-08-04 22:01:03 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 120 ms, Memory: 14324 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -1,17 +1,27 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
-        int[] dp = new int[1001];
-        dp[1] = 1;
-        dp[2] = 3;
-        for (int i = 3; i < N + 1; i++) {
-            dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % 10007;
+        int[] dp = new int[N + 1];
+        dp[1] = mod(1);
+
+        if (N >= 2) {
+            dp[2] = mod(3);
+            for (int i = 3; i <= N; i++) {
+                dp[i] = dp[i - 1] + 2 * dp[i - 2];
+                dp[i] = mod(dp[i]);
+            }
         }
 
         System.out.println(dp[N]);
+    }
+
+    private static int mod(int num) {
+        return num % 10007;
     }
 }
