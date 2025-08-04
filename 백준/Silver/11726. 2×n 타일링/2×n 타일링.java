07892b5a0818e0dddeb9d87b4790fa55e966b182
kimdevspace <158041455+kimdevspace@users.noreply.github.com>
2025-08-04 21:08:19 +0900

    [Silver III] Title: 2×n 타일링, Time: 128 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -5,14 +5,24 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int N = Integer.parseInt(br.readLine());
 
         int[] dp = new int[N + 1];
-        dp[0] = 1;
         dp[1] = 1;
-        for (int i = 2; i <= N; i++) {
-            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+
+        if (N >= 2) {
+            dp[2] = 2;
+            for (int i = 3; i <= N; i++) {
+                dp[i] = mod(dp[i - 1]) + mod(dp[i - 2]);
+            }
+            System.out.println(mod(dp[N]));
+        } else {
+            System.out.println(mod(dp[N]));
         }
-        System.out.println(dp[N]);
+    }
+
+    private static int mod(int num) {
+        return num % 10007;
     }
 }
