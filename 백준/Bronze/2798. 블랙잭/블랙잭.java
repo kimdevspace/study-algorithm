2e64cbf6db3012a9c0224c64759f43e218b95859
kimdevspace <158041455+kimdevspace@users.noreply.github.com>
2025-08-09 19:19:00 +0900

    [Bronze II] Title: 블랙잭, Time: 108 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -1,30 +1,38 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = sc.nextInt();
 
-        int[] arr = new int[N];
+    static int N, M;
+    static int[] cards;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        cards = new int[N];
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            arr[i] = sc.nextInt();
+            cards[i] = Integer.parseInt(st.nextToken());
         }
 
         int ans = 0;
-        int sum = 0;
-        for (int i = 0; i < N - 2; i++) {
+        for (int i = 0; i < N; i++) {
             for (int j = i + 1; j < N; j++) {
                 for (int k = j + 1; k < N; k++) {
-                   if (arr[i] + arr[j] + arr[k] <= M) {
-                       sum = arr[i] + arr[j] + arr[k];
-                   }
-                   if (sum > ans) {
-                       ans = sum;
-                   }
+                    int sum = cards[i] + cards[j] + cards[k];
+                    if (sum <= M) {
+                        ans = Math.max(ans, sum);
+                    }
                 }
             }
         }
+
         System.out.println(ans);
     }
 }
