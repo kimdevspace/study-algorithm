012c4fc31ea13f2e34bf460aa150473a1ba5f0e0
kimdevspace <158041455+kimdevspace@users.noreply.github.com>
2025-08-18 11:18:29 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 120 ms, Memory: 14352 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,36 +1,50 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-
-        int[] arr = new int[N + 1];
-
+        int[] arr = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= N; i++) {
+        for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        int[] dp = new int[N + 1];
-        dp[0] = 0;
+        List<Integer> lis = new ArrayList<>();
 
-        int ans = 0;
-        for (int i = 1; i < N + 1; i++) {
-            for (int j = 0; j < i; j++) {
-                if (arr[i] > arr[j]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
-                    ans = Math.max(ans, dp[i]);
-                }
+        for (int i = 0; i < N; i++) {
+            int pos = bs(lis, arr[i]);
+
+            if (pos == lis.size()) {
+                lis.add(arr[i]);
+            } else {
+                lis.set(pos, arr[i]);
             }
         }
 
-        System.out.println(ans);
+        System.out.println(lis.size());
+    }
+
+    private static int bs(List<Integer> lis, int target) {
+        int left = 0;
+        int right = lis.size();
+
+        while (left < right) {
+            int mid = (left + right) / 2;
+
+            if (lis.get(mid) < target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return left;
     }
 }
