bb1a1093ffae86f382e94daad8d84b55e84c54fa
kimdevspace <158041455+kimdevspace@users.noreply.github.com>
2025-08-19 16:15:08 +0900

    [Silver III] Title: 계단 오르기, Time: 104 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -8,10 +8,12 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
         int[] stairs = new int[N + 1];
-        for (int i = 1; i <= N; i++) {
+        int[] dp = new int[N + 1];
+
+        for (int i = 1; i < N + 1; i++) {
             stairs[i] = Integer.parseInt(br.readLine());
         }
-        
+
         if (N == 1) {
             System.out.println(stairs[1]);
             return;
@@ -21,16 +23,14 @@ public class Main {
             System.out.println(stairs[1] + stairs[2]);
             return;
         }
-
-        int[] dp = new int[N + 1];
+        
         dp[1] = stairs[1];
         dp[2] = stairs[1] + stairs[2];
-
-        for (int i = 3; i <= N; i++) {
+        
+        for (int i = 3; i < N + 1; i++) {
             dp[i] = Math.max(dp[i - 2] + stairs[i], dp[i - 3] + stairs[i - 1] + stairs[i]);
         }
 
         System.out.println(dp[N]);
-
     }
 }
