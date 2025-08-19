4f0e8e445e7a717aad46448524b08fe8f5606704
kimdevspace <158041455+kimdevspace@users.noreply.github.com>
2025-08-19 15:37:49 +0900

    [Silver II] Title: 연속합, Time: 240 ms, Memory: 23956 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -9,20 +9,20 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
         int[] arr = new int[N];
-        int[] dp = new int[N];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        dp[0] = arr[0];
-        int max = arr[0];
+        int currentSum = arr[0];
+        int maxSum = arr[0];
+
         for (int i = 1; i < N; i++) {
-            dp[i] = Math.max(arr[i], arr[i] + dp[i - 1]);
-            max = Math.max(max, dp[i]);
+            currentSum = Math.max(arr[i], arr[i] + currentSum);
+            maxSum = Math.max(maxSum, currentSum);
         }
 
-        System.out.println(max);
+        System.out.println(maxSum);
     }
 }
