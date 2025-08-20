47fcb9d13512d8acb5ff67383827a51a30fea200
kimdevspace <158041455+kimdevspace@users.noreply.github.com>
2025-08-20 18:15:49 +0900

    [Bronze III] Title: 최댓값, Time: 128 ms, Memory: 15976 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -1,27 +1,25 @@
-import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] arr = new int[9];
+
         int max = 0;
-        int ans = 0;
+        int idx = 0;
 
-        for (int i = 0; i < 9; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-        }
+        for (int i = 1; i < 10; i++) {
+            int num = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < 9; i++) {
-            if (arr[i] > max) {
-                max = arr[i];
-                ans = i + 1;
+            if (max < num) {
+                max = num;
+                idx = i;
             }
         }
-        System.out.println(max);
-        System.out.println(ans);
+
+        System.out.println(max + "\n" + idx);
+
     }
 }
