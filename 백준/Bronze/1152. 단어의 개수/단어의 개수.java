91159dc450e4a4d56c71185970820d45a485f535
kimdevspace <158041455+kimdevspace@users.noreply.github.com>
2025-08-20 18:08:52 +0900

    [Bronze II] Title: 단어의 개수, Time: 288 ms, Memory: 51860 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        String[] strings = str.split(" ");
+
+        int ans = 0;
+
+        for (String string : strings) {
+            if (string.isEmpty()) continue;
+            ans++;
+        }
+
+        System.out.println(ans);
+    }
+}
