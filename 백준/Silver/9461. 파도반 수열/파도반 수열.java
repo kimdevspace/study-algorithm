a4511507607cbbf24cfad19ed8067513bbd60334
kimdevspace <158041455+kimdevspace@users.noreply.github.com>
2025-08-21 10:17:00 +0900

    [Silver III] Title: 파도반 수열, Time: 104 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -4,26 +4,27 @@ import java.io.InputStreamReader;
 
 public class Main {
 
+    static long[] dp = new long[101];
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        long[] dp = new long[101];
-        int[] startArr = {0, 1, 1, 1, 2, 2, 3, 4, 5, 7, 9};
-        for (int i = 1; i < startArr.length; i++) {
-            dp[i] = startArr[i];
+        dp[1] = 1;
+        dp[2] = 1;
+        dp[3] = 1;
+        dp[4] = 2;
+        dp[5] = 2;
+        dp[6] = 3;
+
+        for (int i = 7; i < 101; i++) {
+            dp[i] = dp[i - 5] + dp[i - 1];
         }
 
         int T = Integer.parseInt(br.readLine());
-        for (int tc = 0; tc < T; tc++) {
+        for (int tc = 0 ; tc < T; tc++) {
             int N = Integer.parseInt(br.readLine());
-            if (N <= 10) {
-                System.out.println(dp[N]);
-            } else {
-                for (int i = 11; i <= N; i++) {
-                    dp[i] = dp[i - 1] + dp[i - 5];
-                }
-                System.out.println(dp[N]);
-            }
+
+            System.out.println(dp[N]);
         }
     }
 }
