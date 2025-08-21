ae1e037b3c486fcddff162d098436c0f0d0813fd
kimdevspace <158041455+kimdevspace@users.noreply.github.com>
2025-08-21 15:21:19 +0900

    [Bronze V] Title: A+B - 2, Time: 172 ms, Memory: 17752 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        for (int i = 0; i < 2; i++) {
+            sum += sc.nextInt();
+        }
+        
+        System.out.println(sum);
+    }
+}
