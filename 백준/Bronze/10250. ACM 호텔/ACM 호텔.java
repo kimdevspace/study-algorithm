0e578b27406bb81eecbd0a4c345618297bfd88d5
kimdevspace <158041455+kimdevspace@users.noreply.github.com>
2025-08-22 15:27:30 +0900

    [Bronze III] Title: ACM 호텔, Time: 128 ms, Memory: 14764 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
@@ -1,17 +1,31 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
 
         for (int tc = 0; tc < T; tc++) {
-            int H = sc.nextInt();
-            int W = sc.nextInt();
-            int N = sc.nextInt();
+            st = new StringTokenizer(br.readLine());
+            int H = Integer.parseInt(st.nextToken());
+            int W = Integer.parseInt(st.nextToken());
+            int N = Integer.parseInt(st.nextToken());
 
-            if (N % H == 0) System.out.println((H * 100) + (N / H));
-            else System.out.println(((N % H) * 100) + ((N / H) + 1));
+            sb.append(selectedRoom(H, W, N)).append("\n");
         }
+        System.out.println(sb);
+    }
+
+    private static String selectedRoom(int H, int W, int N) {
+        int floor = (N - 1) % H + 1;
+        int room = (N - 1) / H + 1;
+
+        return String.format("%d%02d", floor, room);
     }
 }
