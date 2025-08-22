b31c01e9b29d579b7a3256cbb913997ce3cb9369
kimdevspace <158041455+kimdevspace@users.noreply.github.com>
2025-08-22 15:06:59 +0900

    [Bronze V] Title: 시험 성적, Time: 104 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -1,19 +1,14 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+ 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int a = Integer.parseInt(br.readLine());
-
-        if (a >= 90)
-            System.out.println("A");
-        else if (a >= 80)
-            System.out.println("B");
-        else if (a >= 70)
-            System.out.println("C");
-        else if (a >= 60)
-            System.out.println("D");
-        else
-            System.out.println("F");
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+ 		int A = Integer.parseInt(br.readLine());
+        
+		System.out.print((A>=90)?"A": (A>=80)? "B": (A>=70)? "C": (A>=60)? "D": "F");
+ 
+	}
 }
