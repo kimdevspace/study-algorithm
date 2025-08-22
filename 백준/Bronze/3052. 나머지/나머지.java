b9d2ffedd3bb2ceca5d1e8929747634627f2dd33
kimdevspace <158041455+kimdevspace@users.noreply.github.com>
2025-08-22 14:56:11 +0900

    [Bronze II] Title: 나머지, Time: 100 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -1,30 +1,19 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] arr = new int[10];
+
+        Set<Integer> set = new HashSet<>();
 
         for (int i = 0; i < 10; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+            set.add(Integer.parseInt(br.readLine()) % 42);
         }
 
-        for (int i = 0; i < 10; i++) {
-            arr[i] = arr[i] % 42;
-        }
-
-        int cnt = 0;
-
-        for (int i = 0; i < 10; i++) {
-            for (int j = i + 1; j < 10; j++) {
-                if (arr[i] == arr[j]) {
-                    cnt += 1;
-                    break;
-                }
-            }
-        }
-        System.out.println(10 - cnt);
+        System.out.println(set.size());
     }
 }
