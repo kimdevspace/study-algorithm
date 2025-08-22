51bde8befee9c27f83ad6eeeeda69522bc070b43
kimdevspace <158041455+kimdevspace@users.noreply.github.com>
2025-08-22 14:52:50 +0900

    [Bronze II] Title: 음계, Time: 100 ms, Memory: 14088 KB -BaekjoonHub

--- 백준/Bronze/2920. 음계/음계.java
+++ 백준/Bronze/2920. 음계/음계.java
@@ -1,25 +1,38 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
         int[] arr = new int[8];
+        int[] check = new int[8];
         for (int i = 0; i < 8; i++) {
-            arr[i] = sc.nextInt();
+            arr[i] = Integer.parseInt(st.nextToken());
+            check[i] = arr[i];
         }
-        sc.close();
 
-        String res = "";
+        Arrays.sort(check);
 
-        for (int i = 0; i < arr.length - 1; i++) {
-            if (arr[i] == arr[i + 1] - 1) res = "ascending";
-            else if (arr[i] == arr[i + 1] + 1) res = "descending";
-            else {
-                res = "mixed";
-                break;
+        if (Arrays.equals(arr, check)) {
+            System.out.println("ascending");
+        } else {
+            for (int i = 0; i < check.length / 2; i++) {
+                int temp = check[i];
+                check[i] = check[check.length - 1 - i];
+                check[check.length - 1 - i] = temp;
+            }
+
+            if (Arrays.equals(arr, check)) {
+                System.out.println("descending");
+            } else {
+                System.out.println("mixed");
             }
         }
-        System.out.println(res);
     }
 }
