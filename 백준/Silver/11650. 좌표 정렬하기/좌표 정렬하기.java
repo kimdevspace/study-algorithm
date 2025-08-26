3036ab9c7905ec9776c9e0cc87c66e20db219137
kimdevspace <158041455+kimdevspace@users.noreply.github.com>
2025-08-26 22:48:15 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 1120 ms, Memory: 54812 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -5,35 +5,28 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         int N = Integer.parseInt(br.readLine());
         int[][] arr = new int[N][2];
-
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 2; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.sort(arr, (o1, o2) -> {
-            if (o1[0] == o2[0]) {
-                return o1[1] - o2[1];
+        Arrays.sort(arr, (e1, e2) -> {
+            if (e1[0] == e2[0]) {
+                return e1[1] - e2[1];
             } else {
-                return o1[0] - o2[0];
+                return e1[0] - e2[0];
             }
         });
 
         for (int i = 0; i < N; i++) {
-            for (int j = 0; j < 2; j++) {
-                System.out.print(arr[i][j] + " ");
-            }
-            System.out.println();
+            System.out.println(arr[i][0] + " " + arr[i][1]);
         }
     }
 }
