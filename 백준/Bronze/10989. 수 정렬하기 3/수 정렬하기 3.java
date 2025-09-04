a990152a7ae0d31902782008b10e72c0e8ba6a98
kimdevspace <158041455+kimdevspace@users.noreply.github.com>
2025-09-04 09:53:45 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 2768 ms, Memory: 380976 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -1,28 +1,73 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-// 계수정렬############
+
 public class Main {
+
+    static int[] temp;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
-        int[] cnt = new int[10001];
-        // arr 배열 받기
         int[] arr = new int[N];
+        temp = new int[N];
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(br.readLine());
-            cnt[arr[i]]++;
         }
 
-       
-        StringBuilder sb = new StringBuilder();
-        for (int i = 1; i < 10001; i++) {
-            while (cnt[i] > 0) {
-                sb.append(i).append('\n');
-                cnt[i]--;
+        mergeSort(arr, 0, arr.length - 1);
+
+        for (int i = 0; i < N; i++) {
+            sb.append(arr[i]).append("\n");
+        }
+
+        System.out.println(sb);
+
+    }
+
+    static void mergeSort(int[] arr, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+
+        int mid = (left + right) / 2;
+
+        mergeSort(arr, left, mid);
+
+        mergeSort(arr, mid + 1, right);
+
+        merge(arr, left, mid, right);
+    }
+
+    static void merge(int[] arr, int left, int mid, int right) {
+        copy(arr, left, right);
+
+        int l = left;
+        int r = mid + 1;
+        int idx = left;
+
+        while (l <= mid && r <= right) {
+            if (temp[l] <= temp[r]) {
+                arr[idx++] = temp[l++];
+            } else {
+                arr[idx++] = temp[r++];
             }
         }
-        System.out.println(sb);
+
+        while (l <= mid) {
+            arr[idx++] = temp[l++];
+        }
+
+        while (r <= right) {
+            arr[idx++] = temp[r++];
+        }
+    }
+
+    static void copy(int[] arr, int left, int right) {
+        for (int i = left; i <= right; i++) {
+            temp[i] = arr[i];
+        }
     }
 }
